15a38548a761e66ffeb96713a64a7422250ed2ac
felixp <felix.piskunov@cellebrite.com>
2017-07-19 14:48:24 +0300

    Dragging

--- js/game.js
+++ js/game.js
@@ -7,6 +7,8 @@
         game.load.image('stone', '/assets/towerDefense_tile137.png');
         game.load.image('bush1', '/assets/towerDefense_tile130.png');
         game.load.image('bush2', '/assets/towerDefense_tile134.png');
+        game.load.image('tower-body', '/assets/towerDefense_tile180.png');
+        game.load.image('tower-gun', '/assets/towerDefense_tile250.png');
     }
 
     function create(params) {
@@ -20,7 +22,9 @@
         for (var i = 0; i < 5; i++) {
             var x = parseInt(Math.random() * (15 - 1) + 1) * 64;
             var y = parseInt(Math.random() * (10 - 1) + 1) * 64;
-            game.add.sprite(x, y, 'stone');
+            var stone = game.add.sprite(x, y, 'stone');
+            stone.inputEnabled = true;
+            stone.input.enableDrag(true);
         }
 
         for (var i = 0; i < 3; i++) {
@@ -34,6 +38,20 @@
             var y = parseInt(Math.random() * (10 - 1) + 1) * 64;
             game.add.sprite(x, y, 'bush2');
         }
+
+        var tower = game.add.group();
+        tower.inputEnableChildren = true;
+        tower.onChildInputDown.add((sprite, pointer) => {
+            console.log(sprite);
+            console.log(pointer);
+        }, this);
+        var towerBody = tower.create(10, 540, 'tower-body');
+        var towerGun = tower.create(10, 530, 'tower-gun');
+        towerBody.inputEnabled = true;
+        towerGun.inputEnabled = true;
+        towerBody.input.enableDrag();
+        towerGun.input.enableDrag();
+        
     }
 
     function update(params) {
